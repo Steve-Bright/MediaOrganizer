d849ef729bfdfdbe5bb694aa0a554cfc76185d08
Steve-Bright <swemhtet.net@gmail.com>
2025-09-12 22:14:03 +0800

    xml comment added

--- Media.cs
+++ Media.cs
@@ -5,12 +5,21 @@ using System;
 
 namespace MediaOrganizer
 {
+     /// <summary>
+     /// This is the Media Class containing 3 fields, each having its own property.
+     /// </summary>
     public class Media
     {
         private string _title;
         private int _size;
         private MediaType _type;
 
+         /// <summary>
+         /// Media Constructor that requires three parameters
+         /// </summary>
+         /// <param name="title">Media's Name</param>
+         /// <param name="size">Media Size</param>
+         /// <param name="type">Media Type</param>
         public Media(string title, int size, MediaType type)
         {
             _title = title;
@@ -18,23 +27,41 @@ namespace MediaOrganizer
             _type = type;
         }
 
+         /// <summary>
+         /// Title property of Media Class
+         /// </summary>
+         /// <value>String data type Media Title</value>
         public string Title
         {
             set { _title = value; }
             get { return _title; }
         }
 
+
+         /// <summary>
+         /// Size property of Media Class
+         /// </summary>
+         /// <value>Integer data type Media Size</value>
         public int Size
         {
             set { _size = value; }
             get { return _size; }
         }
 
+
+         /// <summary>
+         /// Type property of Media Class
+         /// </summary>
+         /// <value>MediaType data type Media Type</value>
         public MediaType Type
         {
             get { return _type; }
         }
 
+         /// <summary>
+         /// Play method for Media class that returns respective value for each media type.
+         /// </summary>
+         /// <returns>Respective String value for each media type</returns>
         public string Play()
         {
             string message = null;
