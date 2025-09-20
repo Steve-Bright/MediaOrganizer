91681bbd29809fa724e24e7dfe011c91e1e4ec49
Steve-Bright <swemhtet.net@gmail.com>
2025-09-20 16:59:13 +0800

    Removed unnecessary configuration > More faithful to assignment Added XML Docs

--- Playlist.cs
+++ Playlist.cs
@@ -3,12 +3,18 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace MediaOrganizer
 {
+    /// <summary>
+    /// Plalist Class to manage Media objects (add, remove, get) into PlayList and PlayDateTime.
+    /// </summary>
     public class Playlist
     {
         private List<Media> _mediaList;
         private DateTime _playDateTime;
 
-        //date time (year, month, day) (hour, minute, second)
+        /// <summary>
+        /// Playlist Constructor that requires DateTime type parameter 
+        /// </summary>
+        /// <param name="dateTime">Datetime when the playlist to be played.</param>
         public Playlist(DateTime dateTime)
         {
             _mediaList = new List<Media>();
@@ -16,47 +22,72 @@ namespace MediaOrganizer
 
         }
 
+        /// <summary>
+        /// Playlist Constructor with no parameter. Since DateTime is not provided,
+        /// Default Datetime after an hour will be initialized.
+        /// </summary>
         public Playlist() : this(DateTime.Now.AddHours(1))
         {
 
         }
 
+        /// <summary>
+        /// PlayDateTime property (Set and Get)
+        /// </summary>
+        /// <value>DateTime data type Playlist's playtime</value>
         public DateTime PlayDateTime
         {
             get { return _playDateTime; }
             set { _playDateTime = value; }
         }
 
+        /// <summary>
+        /// Add Media to playlist. Requires Media Object to be added.
+        /// </summary>
+        /// <param name="media">Media type object</param>
         public void AddMedia(Media media)
         {
             _mediaList.Add(media);
         }
 
+        /// <summary>
+        /// Remove Media from playlist. Requires Media Object be removed.
+        /// </summary>
+        /// <param name="media">Media type object</param>
         public void RemoveMedia(Media media)
         {
             _mediaList.Remove(media);
         }
 
+        /// <summary>
+        /// Number of medias listed in playlist.
+        /// </summary>
+        /// <value>Integer type number of medias</value>
         public int MediaCount
         {
             get { return _mediaList.Count; }
         }
 
+        /// <summary>
+        /// Get the specific media from media lists of playlist. Index number as integer type is required.
+        /// </summary>
         public Media this[int i]
         {
             get { return _mediaList[i]; }
         }
 
+        /// <summary>
+        /// Print full details from playlist. If the media playdatetime is less than 10 minutes,
+        /// full details will be printed otherwise, playlist is not ready.
+        /// </summary>
+        /// <returns>Boolean datatype that indicates whether playlist is printed successfully or not.</returns>
         public bool PrintDetails()
         {
-            Console.WriteLine("Playtime: {0}", _playDateTime.ToString(@"MM\/dd\/yyyy HH:mm"));
             if (!(_playDateTime.AddMinutes(-10) > DateTime.Now))
             {
-                int mediaCounter = 0;
+                Console.WriteLine("Playtime: {0}", _playDateTime.ToString(@"MM\/dd\/yyyy HH:mm"));
                 foreach (Media eachMedia in _mediaList)
                 {
-                    mediaCounter++;
-                    Console.WriteLine("Media Type Number {0}", mediaCounter);
                     Console.WriteLine("========================");
                     Console.WriteLine("Media Title: {0}", eachMedia.Title);
                     Console.WriteLine("Media Size: {0}", eachMedia.Size);
--- obj/Debug/net8.0/MediaOrganizer.AssemblyInfo.cs
+++ obj/Debug/net8.0/MediaOrganizer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MediaOrganizer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6f95827595cc177bb7659edae4585e6405df2e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c61bccfceb1b49d5ac8520c361e3a32cb2f9089")]
 [assembly: System.Reflection.AssemblyProductAttribute("MediaOrganizer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MediaOrganizer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
